4d01009cce215a792e2156437cee20b5c3d9c419
mrandypratt <mrandypratt@gmail.com>
2023-03-01 15:41:33 -0600

    Created AuthService and Imported Users Module to validate password

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule],
+  providers: [AuthService],
 })
 
 export class AuthModule {}
