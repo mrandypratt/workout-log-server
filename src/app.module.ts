582c0cf1c3da6eb13cdb451c14a3b11c297eb7a3
mrandypratt <mrandypratt@gmail.com>
2023-03-01 14:59:40 -0600

    Removed inaccurate import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AuthController } from './Auth/auth.controller';
 
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
