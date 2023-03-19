d131e5593cd93a5c12996be702d3ade07590ff5e
mrandypratt <mrandypratt@gmail.com>
2023-03-19 15:14:28 -0500

    Implemented Logger for Port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 
 const PORT = 5000;
 
 async function bootstrap() {
+  const logger = new Logger("BootStrap")
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(PORT);
+  logger.log(`Listening on PORT ${PORT}`);
 }
 bootstrap();
